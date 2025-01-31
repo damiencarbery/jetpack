33b77fcae362c152180f285a037cf1a0b610886a
Nate Weller <hello@nateweller.com>
2025-01-30 17:33:03 -0700

    minor adjustments

--- projects/plugins/protect/src/js/routes/scan/history/index.jsx
+++ projects/plugins/protect/src/js/routes/scan/history/index.jsx
@@ -30,7 +30,11 @@ const HistoryPage = () => {
 					<AdminSection>
 						<Container
 							className={ styles[ 'history-container' ] }
+<<<<<<< HEAD
 							horizontalSpacing={ 3 }
+=======
+							horizontalSpacing={ 5 }
+>>>>>>> cf978476d7 (minor adjustments)
 							horizontalGap={ 4 }
 						>
 							<Col>
--- projects/plugins/protect/src/js/routes/scan/scan-admin-section-hero.tsx
+++ projects/plugins/protect/src/js/routes/scan/scan-admin-section-hero.tsx
@@ -82,6 +82,9 @@ const ScanAdminSectionHero: React.FC = ( { size = 'normal' }: { size?: 'normal' 
 		}
 	}
 
+	// const [ showFixersModal, setShowFixersModal ] = useState( false );
+	// const [ selectedThreats, setSelectedThreats ] = useState();
+
 	const handleShowAutoFixersClick = threatList => {
 		return event => {
 			event.preventDefault();

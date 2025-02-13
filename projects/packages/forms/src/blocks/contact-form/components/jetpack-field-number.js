cab9077db369c52b2968a4f416f001708fbe102e
Mikael Korpela <mikael@ihminen.org>
2025-02-13 14:23:34 +0200

    Forms: add min/max options to number field

--- projects/packages/forms/src/blocks/contact-form/components/jetpack-field-number.js
+++ projects/packages/forms/src/blocks/contact-form/components/jetpack-field-number.js
@@ -1,7 +1,9 @@
 import { useBlockProps } from '@wordpress/block-editor';
 import { createBlock, getDefaultBlockName } from '@wordpress/blocks';
+import { __experimentalNumberControl as NumberControl } from '@wordpress/components'; // eslint-disable-line @wordpress/no-unsafe-wp-apis
 import { createHigherOrderComponent, compose } from '@wordpress/compose';
 import { addFilter } from '@wordpress/hooks';
+import { __ } from '@wordpress/i18n';
 import clsx from 'clsx';
 import { isEmpty } from 'lodash';
 import { useFormStyle } from '../util/form';
@@ -10,7 +12,7 @@ import JetpackFieldControls from './jetpack-field-controls';
 import JetpackFieldLabel from './jetpack-field-label';
 import { useJetpackFieldStyles } from './use-jetpack-field-styles';
 
-const JetpackField = props => {
+const JetpackNumberField = props => {
 	const {
 		attributes,
 		clientId,
@@ -21,9 +23,10 @@ const JetpackField = props => {
 		label,
 		setAttributes,
 		placeholder,
+		min,
+		max,
 		width,
 		insertBlocksAfter,
-		type,
 	} = props;
 
 	const { blockStyle, fieldStyle } = useJetpackFieldStyles( attributes );
@@ -51,9 +54,10 @@ const JetpackField = props => {
 					className="jetpack-field__input"
 					onChange={ e => setAttributes( { placeholder: e.target.value } ) }
 					style={ fieldStyle }
-					type={ type }
+					type="number"
 					value={ placeholder }
-					onClick={ event => type === 'file' && event.preventDefault() }
+					min={ min }
+					max={ max }
 					onKeyDown={ event => {
 						if ( event.defaultPrevented || event.key !== 'Enter' ) {
 							return;
@@ -70,6 +74,48 @@ const JetpackField = props => {
 				setAttributes={ setAttributes }
 				placeholder={ placeholder }
 				attributes={ attributes }
+				hidePlaceholder={ true }
+				extraFieldSettings={ [
+					{
+						index: 1,
+						element: (
+							<NumberControl
+								label={ __( 'Minimum value', 'jetpack-forms' ) }
+								value={ attributes.min }
+								onChange={ value =>
+									setAttributes( {
+										min: value,
+									} )
+								}
+								max={ max } // Value must be less than or equal to the value of the max attribute.
+								__nextHasNoMarginBottom={ true }
+								__next40pxDefaultSize={ true }
+								help={ __(
+									'The minimum value to accept in the input. Leaving empty allows any negative and positive values.',
+									'jetpack-forms'
+								) }
+							/>
+						),
+					},
+					{
+						index: 2,
+						element: (
+							<NumberControl
+								label={ __( 'Maximum value', 'jetpack-forms' ) }
+								value={ attributes.max }
+								onChange={ value =>
+									setAttributes( {
+										max: value,
+									} )
+								}
+								min={ min } // Value must be greater than or equal to the value of the min attribute.
+								__nextHasNoMarginBottom={ true }
+								__next40pxDefaultSize={ true }
+								help={ __( 'The maximum value to accept in the input.', 'jetpack-forms' ) }
+							/>
+						),
+					},
+				] }
 			/>
 		</>
 	);
@@ -88,7 +134,7 @@ export default compose(
 		'fieldBackgroundColor',
 		'borderColor',
 	] )
-)( JetpackField );
+)( JetpackNumberField );
 
 const withCustomClassName = createHigherOrderComponent( BlockListBlock => {
 	return props => {
